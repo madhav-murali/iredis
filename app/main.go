a05854a454087ac10a4ee63fb973d540fb66a951
Madhav Murali <mdvmrli@gmail.com>
2026-04-20 14:30:13 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,21 +22,21 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment the code below to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-
-		go handlepongs(conn)
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
 	}
+	go func() {
+		defer conn.Close()
+		for {
+			handlepongs(conn)
+		}
+	}()
 
 }
