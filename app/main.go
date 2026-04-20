b87dafd7b67fc8ed4202a598ce195d6674313052
Madhav Murali <mdvmrli@gmail.com>
2026-04-20 10:14:50 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -9,6 +10,12 @@ import (
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+func handlepongs(conn net.Conn) {
+	writer := bufio.NewWriter(conn)
+	writer.WriteString("+PONG\r\n")
+	writer.Flush()
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,9 +28,12 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handlepongs(conn)
+
 }
