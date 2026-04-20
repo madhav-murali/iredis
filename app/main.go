09588827aa9d4f973a25b8f627b75443c693fcbf
Madhav Murali <mdvmrli@gmail.com>
2026-04-20 10:15:58 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -28,12 +28,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
-	handlepongs(conn)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handlepongs(conn)
+	}
 
 }
