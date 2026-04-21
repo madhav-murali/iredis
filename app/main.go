9778f7b010b0cd855eea5c95f0f48026c8f9591e
Madhav Murali <mdvmrli@gmail.com>
2026-04-21 17:46:32 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,20 +7,31 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
-func handlepongs(conn net.Conn) {
+// func handlepongs(conn net.Conn) {
+// 	writer := bufio.NewWriter(conn)
+// 	writer.WriteString("+PONG\r\n")
+// 	writer.Flush()
+// }
+
+func handleWrite(conn net.Conn, s string) {
 	writer := bufio.NewWriter(conn)
-	writer.WriteString("+PONG\r\n")
+	writer.WriteString(s)
 	writer.Flush()
 }
 
-// we get it like *number\r\n$number\r\nPING\r\n -> we need to get these values before the other one
+//TODO: make a resp decoder?;
+//func decodeRESP()
 
+// we get it like *number\r\n$number\r\nPING\r\n -> we need to get these values before the other one
+// *number -> number of words in the command like ping has 1, "echo hey" has two
+// this needs to be appended
 func handle(conn net.Conn) {
 	//fmt.Println("Entering handle")
 	defer conn.Close()
@@ -29,23 +40,32 @@ func handle(conn net.Conn) {
 	//writer := bufio.NewWriter(conn)
 
 	for {
-		//msg := scanner.Text()
-		//log.Println("received msg :", msg)
-		numCmds, err := reader.ReadString('\n')
+		numCmds, err := reader.ReadString('\n') // we get *number or the number of words in cmd.
 		if err != nil {
 			fmt.Println("error reading number of commands:", err)
 		}
 		re := regexp.MustCompile(`\d+`)
 		matches := re.FindAllString(numCmds, -1)
 		numCmds_, err := strconv.Atoi(matches[0])
+		cmds := make([]string, numCmds_)
 		for range numCmds_ {
 			_, err := reader.ReadString('\n') //now we need to read the $ number
 			if err != nil {
 				fmt.Println("error reading from client")
 				return
 			}
-			_, err = reader.ReadString('\n')
-			handlepongs(conn)
+			c, err := reader.ReadString('\n')
+			cmds = append(cmds, c)
+			// handlepongs(conn)
+		}
+		if strings.ToLower(cmds[0]) == "echo" {
+			allCmds := strings.Join(cmds[1:], " ")
+			sizeofString := len(allCmds)
+			echoString := "$" + string(sizeofString) + "\r\n" + allCmds + "\r\n"
+			handleWrite(conn, echoString)
+		} else {
+			send := "+PONG\r\n"
+			handleWrite(conn, send)
 		}
 	}
 }
