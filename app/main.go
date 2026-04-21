9d872ddd621a0d00750d8526f5d3276145408496
Madhav Murali <mdvmrli@gmail.com>
2026-04-21 13:52:10 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,36 +5,48 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
-// func handlepongs(conn net.Conn) {
-// 	writer := bufio.NewWriter(conn)
-// 	writer.WriteString("+PONG\r\n")
-// 	writer.Flush()
-// }
+func handlepongs(conn net.Conn) {
+	writer := bufio.NewWriter(conn)
+	writer.WriteString("+PONG\r\n")
+	writer.Flush()
+}
+
+// we get it like *number\r\n$number\r\nPING\r\n -> we need to get these values before the other one
 
 func handle(conn net.Conn) {
 	//fmt.Println("Entering handle")
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
-	writer := bufio.NewWriter(conn)
+	//writer := bufio.NewWriter(conn)
 
 	for {
 		//msg := scanner.Text()
 		//log.Println("received msg :", msg)
-		_, err := reader.ReadString('\n')
+		numCmds, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("error reading from client")
-			return
+			fmt.Println("error reading number of commands:", err)
 		}
-
-		writer.WriteString("+PONG\r\n")
-		writer.Flush()
+		re := regexp.MustCompile(`\d+`)
+		matches := re.FindAllString(numCmds, -1)
+		numCmds_, err := strconv.Atoi(matches[0])
+		for range numCmds_ {
+			_, err := reader.ReadString('\n') //now we need to read the $ number
+			if err != nil {
+				fmt.Println("error reading from client")
+				return
+			}
+			_, err = reader.ReadString('\n')
+			handlepongs(conn)
+		}
 	}
 }
 
